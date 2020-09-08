aceb73a532ca4d994b7b690a29335b3a91d1c98c
zhiwei_home <xiezhiwei0@gmail.com>
2020-09-09 02:03:11 +0900

    基盤をコミット

---
+++ workspace_FQA/src/main/java/com/acep/fqa/WorkspaceFqaApplication.java
@@ -0,0 +1,13 @@
+package com.acep.fqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceFqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceFqaApplication.class, args);
+	}
+
+}
---
+++ workspace_FQA/src/test/java/com/acep/fqa/WorkspaceFqaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.acep.fqa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkspaceFqaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
